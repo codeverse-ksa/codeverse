eb245796bd2d16c7aeaeb1971275abf299bf6481
Muhammad Hamza <codingtimelapseyt@gmail.com>
2024-08-07 15:38:39 +0300

    Margin for sections and About section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Header from "@/components/header";
+import Section from "@/components/section";
 import Image from "next/image";
 
 export default function Home() {
@@ -15,6 +17,19 @@ export default function Home() {
           </div>
         </div>
       </div>
+      <Section>
+        <Header title="About" />
+        <div className="card lg:card-side bg-base-100">
+          <figure>
+            <Image src="" alt="codeverse.org" className="max-w-sm rounded-lg shadow-2xl" width={500} height={500} />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Catchphrase</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet sequi repellendus, id magni cupiditate beatae officiis quos necessitatibus cumque officia pariatur magnam deserunt culpa? Rem perspiciatis soluta cumque tempora nam?</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet sequi repellendus, id magni cupiditate beatae officiis quos necessitatibus cumque officia pariatur magnam deserunt culpa? Rem perspiciatis soluta cumque tempora nam?</p>
+          </div>
+        </div>
+      </Section>
     </div>
   );
 };
--- components/section.tsx
+++ components/section.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 const Section = ({ children }: Props) => {
     return (
-        <div className="flex flex-col items-center gap-y-4">
+        <div className="flex flex-col items-center gap-y-4 mb-8">
             {children}
         </div>
     );
