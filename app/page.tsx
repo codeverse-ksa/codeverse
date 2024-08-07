e7337dae06825225cb5fec04578e364d7ee04f2e
Muhammad Hamza <codingtimelapseyt@gmail.com>
2024-08-07 15:24:04 +0300

    Basic hero page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="m-4">
+      <div className="hero min-h-screen">
+        <div className="hero-content flex-col lg:flex-row">
+          <Image src="" alt="codeverse.org" className="max-w-sm rounded-lg shadow-2xl" width={1000} height={1000} />
+          <div>
+            <h1 className="text-5xl font-bold">Codeverse.org</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+              quasi. In deleniti eaque aut repudiandae et a id nisi.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
