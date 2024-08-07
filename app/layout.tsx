fa4f664562f409c2927942b5159bac8937769447
Muhammad Hamza <codingtimelapseyt@gmail.com>
2024-08-07 15:10:28 +0300

    Navbar component and margin for webpage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +14,10 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
+        <div className="m-4">
+          <Navbar />
+          {children}
+        </div>
       </body>
     </html>
   );
---
+++ components/navbar.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+const Navbar = () => {
+    return (
+        <div className="navbar bg-base-100">
+            <div className="flex-1">
+                <a className="btn btn-ghost text-xl">Codeverse</a>
+            </div>
+            <div className="flex-none">
+                <ul className="menu menu-horizontal px-1 items-center gap-x-4">
+                    <li><Link href="/programs">Programs</Link></li>
+                    <li><button className="btn btn-primary">Join List</button></li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
