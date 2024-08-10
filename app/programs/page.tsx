9571cf7be2c815bbd38daefb03e21f6752543434
Muhammad Hamza <codingtimelapseyt@gmail.com>
2024-08-10 18:11:52 +0300

    Using date as string and showing most recent program

--- app/programs/page.tsx
+++ app/programs/page.tsx
@@ -6,7 +6,7 @@ export default async function Programs() {
     return (
         <div className="mx-0 md:mx-8 lg:mx-16">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                {programs.map(program => <ProgramCard program={program} />)}
+                {programs.reverse().map(program => <ProgramCard program={program} />)}
             </div>
         </div>
     );
--- components/programcard.tsx
+++ components/programcard.tsx
@@ -19,7 +19,7 @@ const ProgramCard = ({ program }: Props) => {
               {program.tags.split(",").map(tag => <span className="badge badge-primary text-white text-xs">{tag}</span>)}
             </div>
             <div className="card-actions justify-between items-center mt-4">
-                <span className="text-gray-500">{(new Date(program.date)).toLocaleDateString("en-US", { day: "numeric", month: "long", year: "numeric" })}</span>
+                <span className="text-gray-500">{program.date}</span>
                 <Link href={program.tally} className="btn btn-primary text-white">Register</Link>
             </div>
         </div>
