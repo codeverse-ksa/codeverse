7ecf8099029c61a2b2a38c1c2745e7732093a5d3
Muhammad Hamza <codingtimelapseyt@gmail.com>
2024-08-10 17:47:20 +0300

    Removing admin features for now

--- app/programs/page.tsx
+++ app/programs/page.tsx
@@ -1,4 +1,3 @@
-import AddProgram from "@/components/addprogram";
 import ProgramCard from "@/components/programcard";
 import prisma from "@/utils/db";
 
@@ -8,9 +7,6 @@ export default async function Programs() {
         <div className="mx-0 md:mx-8 lg:mx-16">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {programs.map(program => <ProgramCard program={program} />)}
-                <div className="w-full h-auto flex justify-center items-center">
-                    <AddProgram />
-                </div>
             </div>
         </div>
     );
